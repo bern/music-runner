300c792f423d9bb0d9a413a5d1ae672c72eaae7c
bern422 <bern422@gmail.com>
2015-12-01 12:23:47 -0500

    cleaned up python script

--- game/android/assets/SongLength.py
+++ game/android/assets/SongLength.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.io.wavfile
 
 def analyze():
-	rate, data = scipy.io.wavfile.read('test3.wav')
+	rate, data = scipy.io.wavfile.read('delilah.wav')
 
 	numFrames = len(data)
 	print(numFrames)
